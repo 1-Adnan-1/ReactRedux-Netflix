2283b93818b6dd759c7b3b16bd35c9470e4718b1
1-adnan-1 <altintasadnan9@gmail.com>
2025-03-06 21:08:04 +0300

    project finished

--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -1,5 +1,17 @@
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className="flex justify-center mt-20">
+      <div className="loader">
+        <div className="loader__inner"></div>
+        <div className="loader__orbit">
+          <div className="loader__dot"></div>
+          <div className="loader__dot"></div>
+          <div className="loader__dot"></div>
+          <div className="loader__dot"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Loader;
